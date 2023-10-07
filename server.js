dd846a12fce4a87f3dad16b00cf9dfeebc54638c
Twinkle Parmar <twinkle@Twinkles-MacBook-Pro.local>
2023-10-07 13:55:36 -0400

    Initial

--- server.js
+++ server.js
@@ -13,6 +13,10 @@ app.use(express.urlencoded({ extended: true })) // for parsing application/x-www
 app.get('/', function (req, res) {
     res.send("<h1>Home Page</h1>")
 })
+//http://localhost:3000/name
+app.get('/name', function (req, res) {
+  res.send("<h1>C0869356-Twinkle Parmar</h1>")
+})
 
 //http://localhost:3000/profile
 app.post('/profile', (req, res) => {
